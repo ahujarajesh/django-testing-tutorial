33efe47399d9a1624bf74544dfc4e52fe8872321
brijesh <brijeshakbari@gmail.com>
2019-07-09 23:02:36 +0530

    Allowed hosts added

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'q^yjpb!z-^5de=foku(-%k=67a8th&u3w6%p=i8n21_2i#rt&#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','3.82.171.238']
 
 # Application definition
 
